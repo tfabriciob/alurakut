c6ca3eb9f59a57b01ef568b44ce10af47ddcc62b
Fabricio Barbosa <fabricio.barbosa@vgxcontactcenter.com.br>
2021-08-24 10:46:32 -0300

    GraphQL, BFF e AJAX

--- pages/index.js
+++ pages/index.js
@@ -45,11 +45,7 @@ function ProfileRelationsBox(propriedades) {
 
 export default function Home() {
     const usuarioAleatorio = 'tfabriciob';
-    const [comunidades, setComunidades] = React.useState([{
-        id: '97946542314697986546313465949',
-        title: 'Eu odeio acordar cedo',
-        image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
-    }]);
+    const [comunidades, setComunidades] = React.useState([]);
     // const comunidades = comunidades[0];
     // const alteradorDeComunidades/setComunidades = comunidades[1];
     // const comunidades = ['Alurakut'];
@@ -72,6 +68,34 @@ export default function Home() {
         .then(function(respostaCompleta) {
             setSeguidores(respostaCompleta);
         })
+
+        // API GraphQL
+        fetch('https://graphql.datocms.com/', {
+            method: 'POST',
+            headers: {
+                'Authorization': '543f702806e6610ea8d19f308b8246',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ "query": `query {
+                allCommunities {
+                  id 
+                  title
+                  imageUrl
+                  creatorSlug
+                }
+              }` })
+            })
+            .then((response) => response.json()) // Pega o retorno do response.json() e já retorna
+            .then((respostaCompleta) => {
+              const comunidadesVindasDoDato = respostaCompleta.data.allCommunities;
+              console.log(comunidadesVindasDoDato)
+              setComunidades(comunidadesVindasDoDato)
+            })
+        // .then(function (response) {
+        //   return response.json()
+        // })
+
     }, [])
 
     console.log('seguidores antes do return', seguidores);
@@ -102,13 +126,26 @@ export default function Home() {
                             console.log('Campo: ', dadosDoForm.get('image'));
 
                             const comunidade = {
-                                id: new Date().toISOString(),
                                 title: dadosDoForm.get('title'),
-                                image: dadosDoForm.get('image'),
+                                imageUrl: dadosDoForm.get('image'),
+                                creatorSlug: usuarioAleatorio,
                             }
 
-                            const comunidadesAtualizadas = [...comunidades, comunidade];
-                            setComunidades(comunidadesAtualizadas)
+                            fetch('/api/comunidades', {
+                                method: 'POST',
+                                headers: {
+                                  'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify(comunidade)
+                              })
+                              .then(async (response) => {
+                                const dados = await response.json();
+                                console.log(dados.registroCriado);
+                                const comunidade = dados.registroCriado;
+                                const comunidadesAtualizadas = [...comunidades, comunidade];
+                                setComunidades(comunidadesAtualizadas)
+                              })
+
                         }}>
                             <div>
                                 <input 
@@ -141,8 +178,8 @@ export default function Home() {
                         {comunidades.map((itemAtual) => {
                             return (
                             <li key={itemAtual.id}>
-                                <a href={`/users/${itemAtual.title}`}>
-                                <img src={itemAtual.image} />
+                                <a href={`/communities/${itemAtual.id}`}>
+                                <img src={itemAtual.imageUrl} />
                                 <span>{itemAtual.title}</span>
                                 </a>
                             </li>
